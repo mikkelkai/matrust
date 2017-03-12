b793caff922b833c50a0c292e3de5c217adf24f5
mikkelkai <mikkelkai@gmail.com>
2017-03-12 09:33:39 +0100

    Rewrite, removes randand renames functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,16 @@
-extern crate rand;
 extern crate num;
 
-use rand::Rng;
-use std::fmt;
 use num::Num;
+use std::fmt;
 
-#[derive(Debug)]
-pub struct Matrix<T> {
+#[derive(Debug, PartialEq)]
+pub struct Matrix<A> {
     rows: usize,
     cols: usize,
-    val: Vec<T>,
+    val: Vec<A>,
 }
 
-impl<T: Clone + Num + fmt::Display> fmt::Display for Matrix<T> {
+impl<A: Clone + fmt::Display> fmt::Display for Matrix<A> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         try!(write!(f, "["));
         for i in 0..self.rows {
@@ -30,39 +28,123 @@ impl<T: Clone + Num + fmt::Display> fmt::Display for Matrix<T> {
     }
 }
 
-impl<T: Clone + Num + std::iter::Sum> Matrix<T> {
-    pub fn new(m: usize, n: usize, value: T) -> Matrix<T> {
+impl<A: Clone> Matrix<A> {
+    // Initialization
+    pub fn new(rows: usize, cols: usize, val: A) -> Matrix<A> {
         Matrix {
-            rows: m,
-            cols: n,
-            val: vec![value; m * n],
+            rows: rows,
+            cols: cols,
+            val: vec![val; rows * cols],
         }
     }
 
-    fn with_val(m: usize, n: usize, val: Vec<T>) -> Matrix<T>{
+    pub fn new_with_val(rows: usize, cols: usize, val: Vec<A>) -> Result<Matrix<A>, &'static str> {
+        if val.len() != rows * cols {
+            return Err("Value length needs to equal rows * cols");
+        }
+        Ok(Matrix {
+            rows: rows,
+            cols: cols,
+            val: val,
+        })
+    }
+
+    // Indexing
+    pub fn index(&self, row: usize, col: usize) -> Result<A, &'static str> {
+        if row >= self.rows || col >= self.cols {
+            return Err("Index out of range");
+        }
+        Ok(self.val[row * self.cols + col].clone())
+    }
+
+    // Inserting
+    pub fn insert(&mut self, row: usize, col: usize, value: A) -> Result<(), &'static str> {
+        if row >= self.rows || col >= self.cols {
+            return Err("Index out of range");
+        }
+        self.val[row * self.cols + col] = value;
+        Ok(())
+    }
+
+    // Dimensions
+    pub fn dimensions(&self) -> (usize, usize) {
+        (self.rows, self.cols)
+    }
+
+    // Mapping
+    pub fn map<F, B>(&self, f: F) -> Matrix<B>
+        where F: Fn(A) -> B
+    {
         Matrix {
-            rows: m,
-            cols: n,
+            rows: self.rows,
+            cols: self.cols,
+            val: self.val.clone().into_iter().map(f).collect(),
+        }
+    }
+
+    pub fn map2<F, B: Clone, C>(&self, m: &Matrix<B>, f: F) -> Result<Matrix<C>, &'static str>
+        where F: Fn(A, B) -> C
+    {
+        if self.dimensions() != m.dimensions() {
+            return Err("Both matricies need to have same dimensions");
+        }
+        Ok(Matrix {
+            rows: self.rows,
+            cols: self.cols,
+            val: (0..self.rows * self.cols)
+                .map(|i| f(self.val[i].clone(), m.val[i].clone()))
+                .collect(),
+        })
+    }
+
+    pub fn transpose(&self) -> Matrix<A> {
+        let mut val = Vec::with_capacity(self.rows * self.cols);
+        for i in 0..self.cols {
+            for j in 0..self.rows {
+                val.push(self.val[j * self.cols + i].clone())
+            }
+        }
+
+        Matrix {
+            rows: self.cols,
+            cols: self.rows,
             val: val,
         }
     }
 
-    pub fn apply_fn<F>(&self, f: F) -> Matrix<T>
-        where F: Fn(T) -> T {
-        Matrix {
+    // Applying
+    /*pub fn apply<F: Clone, B: Clone>(&self, f: Matrix<Box<F>>) -> Result<Matrix<B>, &'static str>
+        where F: Fn(A) -> B
+    {
+        if self.dimensions() == f.dimensions() {
+            return Err("Both matricies need to have same dimensions");
+        }
+        Ok(Matrix {
             rows: self.rows,
             cols: self.cols,
-            val: self.val.clone().into_iter().map(|x| f(x)).collect()
-        }
+            val: (0..self.rows * self.cols).map(|i| f.val[i](self.val[i].clone())).collect(),
+        })
+    }*/
+}
+
+impl<A: Clone + Num> Matrix<A> {
+    pub fn add(&self, m: &Matrix<A>) -> Result<Matrix<A>, &'static str> {
+        self.map2(m, (|x, y| x + y))
     }
 
-    pub fn scale(&self, scalar: T) -> Matrix<T> {
-        self.apply_fn((|x| x * scalar.clone()))
+    pub fn sub(&self, m: &Matrix<A>) -> Result<Matrix<A>, &'static str> {
+        self.map2(m, (|x, y| x - y))
     }
 
-    pub fn mult_vec(&self, v: &Vec<T>) -> Result<Vec<T>, &str> {
+    pub fn scale(&self, n: A) -> Matrix<A> {
+        self.map(|x| x * n.clone())
+    }
+}
+
+impl<A: Clone + Num + std::iter::Sum> Matrix<A> {
+    pub fn vec_mult(&self, v: &Vec<A>) -> Result<Vec<A>, &'static str> {
         if v.len() != self.cols {
-            return Err("Vector length must equal matrix column length")
+            return Err("Vector length must equal matrix column length");
         }
         Ok((0..self.rows)
             .map(|i| {
@@ -72,161 +154,148 @@ impl<T: Clone + Num + std::iter::Sum> Matrix<T> {
             })
             .collect())
     }
+}
 
-    pub fn transpose(&self) -> Matrix<T> {
-        let mut val = Vec::with_capacity(self.rows * self.cols);
-        for i in 0..self.cols {
-            for j in 0..self.rows {
-                val.push(self.val[j * self.cols + i].clone())
-            }
-        }
+#[cfg(test)]
+mod test {
+    use super::*;
 
-        Matrix::with_val(self.cols, self.rows, val)
+    #[test]
+    fn new() {
+        let m = Matrix::new(2, 2, 0);
+        let em = Matrix {
+            rows: 2,
+            cols: 2,
+            val: vec![0, 0, 0, 0],
+        };
+        assert_eq!(m.val, em.val);
+
+        let m2 = Matrix::new(3, 4, 1);
+        let em2 = Matrix {
+            rows: 3,
+            cols: 4,
+            val: vec![1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        };
+        assert_eq!(m2, em2);
     }
 
-    pub fn index(&self, m: usize, n: usize) -> Result<T, &str> {
-        if m > self.rows || n > self.cols {
-            return Err("Index out of range")
-        }
-        Ok(self.val[m * self.cols + n].clone())
+    #[test]
+    fn new_with_val() {
+        let v = vec![1, 2, 3, 4, 5, 6];
+        let m = Matrix::new_with_val(2, 3, v.clone()).unwrap();
+        assert_eq!(m.val, v);
+        // let m = matrix![1, 2, 3; 4, 5, 6]; the dreams that never came true
+        assert!(Matrix::new_with_val(3, 3, vec![1]).is_err());
     }
 
-    pub fn dimensions(&self) -> (usize, usize) {
-        (self.rows, self.cols)
+    #[test]
+    fn index() {
+        let m = Matrix::new_with_val(2, 2, vec![1, 2, 3, 4]).unwrap();
+        assert_eq!(m.index(0, 0).unwrap(), 1);
+        assert_eq!(m.index(0, 1).unwrap(), 2);
+        assert_eq!(m.index(1, 0).unwrap(), 3);
+        assert_eq!(m.index(1, 1).unwrap(), 4);
+        assert!(m.index(2, 0).is_err());
+        assert!(m.index(0, 2).is_err());
     }
 
-    pub fn mult_apply_fn<F>(&self, other: &Matrix<T>, f: F) -> Result<Matrix<T>, &str> 
-    where F: Fn(T, T) -> T{
-        if self.dimensions() != other.dimensions() {
-            return Err("Matricies must be same size")
-        }
-        Ok(Matrix::with_val(self.rows, self.cols, (0..self.val.len()).map(|i| f(self.val[i].clone(), other.val[i].clone())).collect()))
+    #[test]
+    fn insert() {
+        let mut m = Matrix::new(2, 2, 0);
+        m.insert(0, 1, 42).unwrap();
+        assert_eq!(m.index(0, 1).unwrap(), 42);
     }
 
-    pub fn add(&self, other: &Matrix<T>) -> Result<Matrix<T>, &str> {
-        self.mult_apply_fn(other, (|x, y| x + y))
+    #[test]
+    fn dimensions() {
+        assert_eq!(Matrix::new(2, 2, 0).dimensions(), (2, 2));
+        assert_eq!(Matrix::new(22, 43, 0).dimensions(), (22, 43));
     }
 
-    pub fn subtract(&self, other: &Matrix<T>) -> Result<Matrix<T>, &str> {
-        self.mult_apply_fn(other, (|x, y| x - y))
+    #[test]
+    fn map() {
+        let m = Matrix::new_with_val(2, 2, vec![1, 2, 3, 4]).unwrap();
+        let m2 = m.map(|x| x * 2);
+        let m3 = Matrix::new_with_val(2, 2, vec![2, 4, 6, 8]).unwrap();
+        assert_eq!(m2, m3);
     }
 
-    pub fn push(&mut self, m: usize, n: usize, val: T) -> Result<(), &str> {
-        if m > self.rows || n > self.cols {
-            return Err("Index out of range")
-        }
-        self.val[m*self.cols+n] = val;
-        Ok(())
+    #[test]
+    fn map2() {
+        let m = Matrix::new_with_val(2, 2, vec![1, 2, 3, 4]).unwrap();
+        let m2 = Matrix::new_with_val(2, 2, vec![1, 2, 3, 4]).unwrap();
+        let m3 = m.map2(&m2, |x, y| x * y).unwrap();
+        let m4 = Matrix::new_with_val(2, 2, vec![1, 4, 9, 16]).unwrap();
+        assert_eq!(m3, m4);
+
+        let m5 = Matrix::new(3, 3, 0);
+        assert!(m.map2(&m5, (|x, y| y + x)).is_err());
     }
-}
 
-impl<T: Clone + Num + std::iter::Sum + rand::Rand> Matrix<T> {
-    pub fn rand(m: usize, n: usize) -> Matrix<T> {
-        let mut rng = rand::thread_rng();
-        Matrix::with_val(m, n, (0..m * n).map(|_| rng.gen()).collect())
+    #[test]
+    fn add() {
+        let m = Matrix::new_with_val(2, 2, vec![1, 2, 3, 4]).unwrap();
+        let m2 = Matrix::new_with_val(2, 2, vec![1, 2, 3, 4]).unwrap();
+        let m3 = m.add(&m2).unwrap();
+        let m4 = Matrix::new_with_val(2, 2, vec![2, 4, 6, 8]).unwrap();
+        assert_eq!(m3, m4);
+
+        let m5 = Matrix::new(3, 3, 0);
+        assert!(m.add(&m5).is_err());
     }
-}
 
-impl<T: Clone + Num> PartialEq for Matrix<T> {
-    fn eq (&self, other: &Matrix<T>) -> bool {
-        self.rows == other.rows && self.cols == other.cols && self.val == other.val
+    #[test]
+    fn sub() {
+        let m = Matrix::new_with_val(2, 2, vec![1, 2, 3, 4]).unwrap();
+        let m2 = Matrix::new_with_val(2, 2, vec![1, 2, 3, 4]).unwrap();
+        let m3 = m.sub(&m2).unwrap();
+        let m4 = Matrix::new(2, 2, 0);
+        assert_eq!(m3, m4);
+
+        let m5 = Matrix::new(3, 3, 0);
+        assert!(m.sub(&m5).is_err());
     }
-}
 
-#[test]
-fn test_new() {
-    let m = Matrix::new(2, 2, 1.0);
-    assert_eq!(m, Matrix{rows: 2, cols: 2, val: vec![1.0, 1.0, 1.0, 1.0]});
-}
+    #[test]
+    fn scale() {
+        let m = Matrix::new_with_val(2, 2, vec![1, 2, 3, 4]).unwrap();
+        let m2 = m.scale(2);
+        let m3 = Matrix::new_with_val(2, 2, vec![2, 4, 6, 8]).unwrap();
+        assert_eq!(m2, m3);
+    }
 
-#[test]
-fn test_rand() {
-    let m = Matrix::rand(2, 2);
-    assert_eq!(m.rows, 2);
-    assert_eq!(m.cols, 2);
-    assert!(0.0 <= m.val[0] && m.val[0] <= 1.0);
-    assert!(0.0 <= m.val[0] && m.val[1] <= 1.0);
-    assert!(0.0 <= m.val[0] && m.val[2] <= 1.0);
-    assert!(0.0 <= m.val[0] && m.val[3] <= 1.0);
-}
+    #[test]
+    fn vec_mult() {
+        let m = Matrix::new(3, 4, 3);
+        let v = vec![1, 2, 3, 4];
+        let v2 = m.vec_mult(&v).unwrap();
+        let v3 = vec![30, 30, 30];
+        assert_eq!(v2, v3);
+    }
 
-#[test]
-fn test_with_val() {
-    let m = Matrix::with_val(2, 2, vec![1.0, 2.0, 3.0, 4.0]);
-    assert_eq!(m, Matrix{rows: 2, cols: 2, val: vec![1.0, 2.0, 3.0, 4.0]});
-}
+    #[test]
+    fn transpose() {
+        let m = Matrix::new_with_val(2, 3, vec![1, 2, 3, 4, 5, 6]).unwrap();
+        let m2 = m.transpose();
+        assert_eq!(m2.dimensions(), (3, 2));
+        let m3 = Matrix::new_with_val(3, 2, vec![1, 4, 2, 5, 3, 6]).unwrap();
+        assert_eq!(m2, m3);
+    }
 
-#[test]
-fn test_apply_fn() {
-    let m = Matrix::with_val(2, 2, vec![1.0, 2.0, 3.0, 4.0]).apply_fn((|x| x*x));
-    assert_eq!(m, Matrix{rows: 2, cols: 2, val: vec![1.0, 4.0, 9.0, 16.0]});
-}
+    /* Closures pls
+    #[test]
+    fn apply() {
+        let m = Matrix::new_with_val(2, 2, vec![1, 2, 3, 4]).unwrap();
+        let m2 = Matrix::new_with_val(2,
+                                      2,
+                                      vec![Box::new((|x| x * 4)),
+                                           Box::new((|x| x / 2)),
+                                           Box::new((|x| x * 2)),
+                                           Box::new((|x| x / 2))])
+            .unwrap();
+        let m3 = m.apply(m2).unwrap();
+        let m4 = Matrix::new_with_val(2, 2, [4, 1, 6, 2]);
+        assert_eq!(m3, m4);
+    }*/
 
-#[test]
-fn test_scale() {
-    let m = Matrix::with_val(2, 2, vec![1.0, 2.0, 3.0, 4.0]).scale(2.0);
-    assert_eq!(m, Matrix{rows: 2, cols: 2, val: vec![2.0, 4.0, 6.0, 8.0]});
-}
-
-#[test]
-fn test_mult_vec() {
-    let m = Matrix::with_val(2, 2, vec![1.0, 2.0, 3.0, 4.0]);
-    let v = m.mult_vec(&vec![2.0, 1.0]);
-    assert_eq!(v, Ok(vec![4.0, 10.0]));
-}
-
-#[test]
-fn test_transpose() {
-    let m = Matrix::with_val(2, 2, vec![1.0, 2.0, 3.0, 4.0]);
-    assert_eq!(m.transpose(), Matrix::with_val(2, 2, vec![1.0, 3.0, 2.0, 4.0]));
-    let m = Matrix::with_val(3, 3, vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0]);
-    assert_eq!(m.transpose(), Matrix::with_val(3, 3, vec![1.0, 4.0, 7.0, 2.0, 5.0, 8.0, 3.0, 6.0, 9.0]));
-    let m = Matrix::with_val(3, 2, vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0]);
-    assert_eq!(m.transpose(), Matrix::with_val(2, 3, vec![1.0, 3.0, 5.0, 2.0, 4.0, 6.0]));
-}
-
-#[test]
-fn test_index() {
-    let m = Matrix::with_val(2, 2, vec![1.0, 2.0, 3.0, 4.0]);
-    assert_eq!(m.index(0, 0), Ok(1.0));
-    assert_eq!(m.index(0, 1), Ok(2.0));
-    assert_eq!(m.index(1, 0), Ok(3.0));
-    assert_eq!(m.index(1, 1), Ok(4.0));
-}
-
-#[test]
-fn test_dimensions() {
-    assert_eq!(Matrix::new(1, 1, 1.0).dimensions(), (1, 1));
-    assert_eq!(Matrix::new(1, 2, 1.0).dimensions(), (1, 2));
-    assert_eq!(Matrix::new(2, 1, 1.0).dimensions(), (2, 1));
-    assert_eq!(Matrix::new(2, 2, 1.0).dimensions(), (2, 2));
-}
-
-#[test]
-fn test_add() {
-    let m1 = Matrix::with_val(2, 3, vec![2.0, 4.0, 5.0, 1.0, 2.0, 3.0]);
-    let m2 = Matrix::with_val(2, 3, vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0]);
-    assert_eq!(m1.add(&m2), Ok(Matrix::with_val(2, 3, vec![3.0, 6.0, 8.0, 5.0, 7.0, 9.0])));
-}
-
-#[test]
-fn test_subtraction() {
-    let m1 = Matrix::with_val(2, 3, vec![2.0, 4.0, 5.0, 1.0, 2.0, 3.0]);
-    let m2 = Matrix::with_val(2, 3, vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0]);
-    assert_eq!(m1.subtract(&m2), Ok(Matrix::with_val(2, 3, vec![1.0, 2.0, 2.0, -3.0, -3.0, -3.0])));
-}
-
-#[test]
-fn test_push() {
-    let mut m = Matrix::new(3,3, 1.0);
-    m.push(0, 2, 2.0);
-    assert_eq!(m.index(0, 2), Ok(2.0));
-    m.push(2, 2, 2.0);
-    assert_eq!(m.index(2, 2), Ok(2.0));
-}
-
-#[test]
-fn test_display() {
-    let m: Matrix<f64> = Matrix::rand(2, 3);
-    println!("{}", m);
 }
